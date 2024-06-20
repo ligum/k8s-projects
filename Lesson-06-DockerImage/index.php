7d501959fdfb054da22197fd0bd56e6776f62ef6
Vladimir Ligum <90089906+ligum@users.noreply.github.com>
2024-06-20 09:30:08 +0300

    Update index.php
    ok

--- Lesson-06-DockerImage/index.php
+++ Lesson-06-DockerImage/index.php
@@ -2,6 +2,6 @@
 $ip_server = $_SERVER['SERVER_ADDR'];
 
 echo "<h1>Hello From Kubernetes</h1><br>";
-echo "Server IP Address is: $ip_server", "<br><p>";
-echo "Made by <font color=blue>Denis Astahov";
+echo "<p>Server IP Address is: $ip_server"</p><br>";
+echo "Made by <font color=blue>VOVA";
 ?>
